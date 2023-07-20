00e281fb0caec328efc4562189636ca1b708735a
Patrick Nilan <nilan.patrick@gmail.com>
2023-07-19 20:40:19 -0700

    Added google maps react

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,14 @@
 import React, {useState} from 'react';
-
+import { GoogleMap, useLoadScript, Loader } from '@react-google-maps/api';
+import { TOKEN } from '../config';
 
 const App = () => {
 
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: TOKEN,
+  });
+
+  if (!isLoaded) { return (<>Hello mapped world</>); }
   return (
     <>Hello mapped world</>
   );
